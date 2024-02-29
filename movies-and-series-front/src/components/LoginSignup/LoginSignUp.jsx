f7c14abef2b1fa551be099def0b07ce80a596856
Natalia392 <ntorrejonvalencia@gmail.com>
2024-02-29 13:40:44 -0300

    feat and style: create login and signup form Add styles for the general background and the form

--- movies-and-series-front/src/components/LoginSignup/LoginSignUp.jsx
+++ movies-and-series-front/src/components/LoginSignup/LoginSignUp.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignUp.css';
 
 const LoginSignUp = () => {
+
+  const [action, setAction] = useState('Sign Up');
+
   return (
-    <div>LoginSignUp</div>
+    <div className='form-container'>
+      <div className='container-title'>{action}</div>
+      <form action="">
+        <div className='input-sign-in-container'>
+          <label htmlFor="username">Username</label> <br />
+          <input className='inputs' type="text" name='username' required />
+        </div>
+        <div className='input-sign-in-container'>
+          <label htmlFor="password">Password</label> <br />
+          <input className='inputs' type="password" name='password' required />
+        </div>
+        <div className='submit-container'>
+          <button className={action === 'Login' ? 'submit grey' : 'submit'} onClick={() => { setAction('Sign Up') }}>Sign Up</button>
+          <button className={action === 'Sign Up' ? 'submit grey' : 'submit'} onClick={() => { setAction('Login') }}>Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
