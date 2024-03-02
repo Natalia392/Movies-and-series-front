dcbffe526ee2b320fdaf53de01413328049de89b
Natalia392 <ntorrejonvalencia@gmail.com>
2024-03-01 21:41:53 -0300

    feat and fix: login Login and signup function is refactored. Upon login, user is redirected to home.

--- movies-and-series-front/src/components/Views/LoginSignup/LoginSignUp.jsx
+++ movies-and-series-front/src/components/Views/LoginSignup/LoginSignUp.jsx
@@ -1,82 +1,73 @@
 import React, { useState } from 'react';
-// import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './LoginSignUp.css';
 import Swal from 'sweetalert2';
 import { loginUser, signUpUser } from '../../../services/authService';
 
 const LoginSignUp = () => {
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const [action, setAction] = useState('Sign Up');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [showConfirmPassword, setShowConfirmPassword] = useState(true);
+  // const [showConfirmPassword, setShowConfirmPassword] = useState(true);
 
   const toggleAction = (newAction) => {
     setAction(newAction);
-    if (newAction === 'Sign Up') {
-      setShowConfirmPassword(true);
-    } else {
-      setShowConfirmPassword(false);
-    }
   };
 
-  const handleLogin = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (action === 'Sign Up') {
-      if (password !== confirmPassword) {
-        Swal.fire({
-          title: 'Error!',
-          text: 'The passwords do not match',
-          icon: 'error',
-          confirmButtonText: 'Accept',
-        });
+    try {
+      if (action === 'Sign Up') {
+        await handleSignUp();
       } else {
-        try {
-          await signUpUser(username, password);
-          console.log('success');
-        } catch(error) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'There was a problem with registration',
-            icon: 'error',
-            confirmButtonText: 'Accept'
-          });
-        }
+        await handleLogin();
       }
-    } else {
-      try {
-        const response = await loginUser(username, password);
-        if (response) {
-          console.log(response);
-          // localStorage.setItem('userId', response.data.id);
-          // navigate('/dashboard');
-        } else {
-          console.error();
-          Swal.fire({
-            title: 'Error!',
-            text: 'Incorrect Credentials.',
-            icon: 'error',
-            confirmButtonText: 'Accept',
-          });
-        }
-      } catch (error) {
-        console.error('Error during login:', error);
-        Swal.fire({
-          title: 'Error!',
-          text: 'There was a problem to login.',
-          icon: 'error',
-          confirmButtonText: 'Accept',
-        });
-      }
+    } catch (error) {
+      handleError(error);
     }
+  };
+  
+  const handleSignUp = async () => {
+    if (password !== confirmPassword) {
+      throw new Error('The passwords do not match');
+    }
+    await signUpUser(username, password);
+    setAction('Login');
+    Swal.fire({
+      title: 'Success!',
+      text: 'User created, now you can Login',
+      icon: 'success',
+      confirmButtonText: 'Accept'
+    });
+  };
+  
+  const handleLogin = async () => {
+    const response = await loginUser(username, password);
+    if (response) {
+      console.log(response);
+      navigate('/home');
+    } else {
+      throw new Error('Incorrect Credentials.');
+    }
+  };
+  
+  const handleError = (error) => {
+    console.error('Error:', error);
+    Swal.fire({
+      title: 'Error!',
+      text: error.message || 'There was a problem',
+      icon: 'error',
+      confirmButtonText: 'Accept'
+    });
   };
 
   return (
     <main>
       <div className='form-container'>
         <div className='container-title'>{action}</div>
-        <form action="">
+        <form onSubmit={handleSubmit} action="">
           <div className='input-sign-in-container'>
             <label htmlFor="username"></label>
             <input
@@ -100,7 +91,7 @@ const LoginSignUp = () => {
               onChange={e => setPassword(e.target.value)}
               required />
           </div>
-          {showConfirmPassword && (
+          {action === 'Sign Up' && (
             <div className='input-sign-in-container'>
               <label htmlFor="confirmPassword"></label>
               <input
@@ -114,21 +105,21 @@ const LoginSignUp = () => {
             </div>
           )}
           <div>
-            <button type='submit' className='submit' onClick={handleLogin}>Enter</button>
+            <button type='submit' className='submit'>Enter</button>
           </div>
           <div className='submit-container'>
-            <button 
-            type='button'
-            className={action === 'Login' ? 'toggle grey' : 'toggle'} 
-            onClick={() => toggleAction('Sign Up')}
+            <button
+              type='button'
+              className={action === 'Login' ? 'toggle grey' : 'toggle'}
+              onClick={() => toggleAction('Sign Up')}
             >
               Sign Up
             </button>
 
-            <button 
-            type='button'
-            className={action === 'Sign Up' ? 'toggle grey' : 'toggle'} 
-            onClick={() => toggleAction('Login')}
+            <button
+              type='button'
+              className={action === 'Sign Up' ? 'toggle grey' : 'toggle'}
+              onClick={() => toggleAction('Login')}
             >
               Login
             </button>
