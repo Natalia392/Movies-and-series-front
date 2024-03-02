5eba99d41fcefe4c4db5ce49ac23f53a61ada7a6
Natalia392 <ntorrejonvalencia@gmail.com>
2024-03-02 16:27:41 -0300

    Feat: implement route protection for auth

--- movies-and-series-front/src/App.js
+++ movies-and-series-front/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import withAuth from './hoc/withAuth.jsx';
 import LoginSignUp from './components/Views/LoginSignup/LoginSignUp.jsx';
 import Home from './components/Views/Home/Home.jsx';
+
+const AuthenticatedHome = withAuth(Home);
 
 function App() {
   return (
     <Router>
-      <Routes>
+      <Switch>
         <Route path='/' element={<LoginSignUp />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
+        <Route path='/home' element={<AuthenticatedHome />} />
+      </Switch>
     </Router>
   );
 }
