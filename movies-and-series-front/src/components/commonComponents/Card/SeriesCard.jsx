46d1abdcf67efdc9c94ade20f5aa0a319a609cc1
Natalia392 <ntorrejonvalencia@gmail.com>
2024-03-09 14:41:00 -0300

    style: create card style.
    Basic card and implement it for series card.

--- movies-and-series-front/src/components/commonComponents/Card/SeriesCard.jsx
+++ movies-and-series-front/src/components/commonComponents/Card/SeriesCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import './CardStyles.css';
 
 function SeriesCard({ series }) {
   const imgURL = "https://image.tmdb.org/t/p/w500";
 
   return (
     <div className="series-card" id={series.id}>
-      imagen, nombre, género, fecha de lanzamiento y calificación
-      <img src={`${imgURL + series.poster_path}`} alt={series.name} />
-      <h2>{series.name}</h2>
-      <p>{series.overview}</p>
-      <p>calificación: {series.vote_average}</p>
-      {/* <p>First Air Date: {series.firstAirDate}</p> */}
+      <figure className='poster-fig'>
+        <img src={`${imgURL + series.poster_path}`} alt={series.name} />
+      </figure>
+      <h2 className='media-title'>{series.name}</h2>
+      {/* <p className='media-description'>{series.overview}</p> */}
+      <p className='calification'>Calification: {series.vote_average}</p>
+      <div className='genres-container'>
+        <p className='genres-list'></p>
+      </div>
     </div>
   )
 }
