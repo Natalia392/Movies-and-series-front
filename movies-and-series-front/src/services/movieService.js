07d66fb74d889c3e1eac0166ad14d50370997bd2
Natalia392 <ntorrejonvalencia@gmail.com>
2024-03-09 12:24:05 -0300

    refactor: fetch movies and series in one function

--- movies-and-series-front/src/services/movieService.js
+++ movies-and-series-front/src/services/movieService.js
@@ -1,32 +1,39 @@
 import { useState, useEffect } from 'react';
+
 const apiKey = process.env.REACT_APP_API_KEY;
 
-const GetMoviesList = () => {
-  const [movies, setMovies] = useState([]);
+const fetchData = async (endpoint) => {
+  try {
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`
+      }
+    });
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+};
+
+export const useMediaList = (mediaType) => {
+  const [mediaList, setMediaList] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${apiKey}`
-          }
-        });
-        const data = await response.json();
-        setMovies(data.results);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    const endpoint = mediaType === 'movies' ?
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc' :
+      'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
 
-    fetchData();
-  }, []);
+    fetchData(endpoint).then(data => {
+      setMediaList(data);
+      setLoading(false);
+      console.log(data);
+    });
+  }, [mediaType]);
 
-  return { movies, loading };
+  return { mediaList, loading };
 };
-
-export default GetMoviesList;
