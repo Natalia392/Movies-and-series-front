41a13c28313b186cab8903b49debfe86894d6737
Natalia392 <ntorrejonvalencia@gmail.com>
2024-03-10 20:42:08 -0300

    feat: create button to add favorites and style it

--- movies-and-series-front/src/components/commonComponents/Card/MovieCard.jsx
+++ movies-and-series-front/src/components/commonComponents/Card/MovieCard.jsx
@@ -8,11 +8,11 @@ const MovieCard = ({ movie, genres }) => {
       .map(genreId => genres.find(genre => genre.id === genreId))
       .filter(genre => genre)
       .map(genre => genre.name);
-  
+
     if (foundGenres.length === 0) {
       return 'not available';
     }
-  
+
     return foundGenres.join(', ');
   };
 
@@ -21,11 +21,18 @@ const MovieCard = ({ movie, genres }) => {
       <figure className="poster-fig">
         <img src={`${imgURL + movie.poster_path}`} alt={movie.name} />
       </figure>
-      <h2 className="media-title">{movie.title}</h2>
-      <p className="release-date">Release Date: {movie.release_date}</p>
-      <p>Calification: {movie.vote_average}</p>
-      <div className="genres-container">
-        <p className="genres-list">Genres: {getGenres()}</p>
+      <div className='bottom-div'>
+        <div className='media-info'>
+          <h2 className="media-title">{movie.title}</h2>
+          <p className="release-date">Release Date: {movie.release_date}</p>
+          <p>Calification: {movie.vote_average}</p>
+          <div className="genres-container">
+            <p className="genres-list">Genres: {getGenres()}</p>
+          </div>
+        </div>
+        <div className='like-div'>
+          <button className='like-btn'>Save</button>
+        </div>
       </div>
     </div>
   );
--- movies-and-series-front/src/components/commonComponents/Card/SeriesCard.jsx
+++ movies-and-series-front/src/components/commonComponents/Card/SeriesCard.jsx
@@ -9,11 +9,11 @@ function SeriesCard({ series, genres }) {
       .map(genreId => genres.find(genre => genre.id === genreId))
       .filter(genre => genre)
       .map(genre => genre.name);
-  
+
     if (foundGenres.length === 0) {
       return 'not available';
     }
-  
+
     return foundGenres.join(', ');
   };
 
@@ -22,11 +22,17 @@ function SeriesCard({ series, genres }) {
       <figure className='poster-fig'>
         <img src={`${imgURL + series.poster_path}`} alt={series.name} />
       </figure>
-      <h2 className='media-title'>{series.name}</h2>
-      {/* <p className='media-description'>{series.overview}</p> */}
-      <p className='calification'>Calification: {series.vote_average}</p>
-      <div className='genres-container'>
-        <p className='genres-list'>Genres: {getGenres()}</p>
+      <div className='bottom-div'>
+        <div className='media-info'>
+          <h2 className='media-title'>{series.name}</h2>
+          <p className='calification'>Calification: {series.vote_average}</p>
+          <div className='genres-container'>
+            <p className='genres-list'>Genres: {getGenres()}</p>
+          </div>
+        </div>
+        <div className='like-div'>
+          <button className='like-btn' type='button'>Save</button>
+        </div>
       </div>
     </div>
   )
